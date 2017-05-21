ffc07ec0df95a6a302249ecf0121d379cf723068
Adrian Preuß <info@adi-code.de>
2017-05-21 18:57:52 +0200

    Split editor for custom files (code, image, textures)

--- Source/Classes/Editor.js
+++ Source/Classes/Editor.js
@@ -2,6 +2,7 @@ module.exports = (function Editor(IDE) {
 	const _IDE		= IDE;
 	const IPC		= require('electron').ipcRenderer;
 	const _path		= require('path');
+	const Klei		= require('../Classes/Klei/Texture.js');
 	var _element	= null;
 	var _editor		= null;
 	var _editors	= null;
@@ -9,6 +10,7 @@ module.exports = (function Editor(IDE) {
 	var _tabs		= null;
 	var _file		= null;
 	var _close		= null;
+	var _type		= null;
 	var _changed	= false;
 	
 	this.init = function init(file, language) {
@@ -19,6 +21,7 @@ module.exports = (function Editor(IDE) {
 		_tab					= document.createElement('editor-tab');
 		_close					= document.createElement('tab-close');
 		_element.id				= _file.file;
+		_type					= _file.type;
 		
 		this.setTitle(_path.basename(_file.file), false);
 		document.querySelector('ui-menu button[data-command="close"]').removeAttribute('disabled');
@@ -35,55 +38,75 @@ module.exports = (function Editor(IDE) {
 		_tabs.appendChild(_tab);
 		_editors.appendChild(_element);
 		
-		monaco.languages.registerCompletionItemProvider('lua', {
-			triggerCharacters:		["."],
-			provideCompletionItems: function(model, position) {
-				var textUntilPosition	= model.getValueInRange({
-					startLineNumber: 1,
-					startColumn: 1,
-					endLineNumber: position.lineNumber,
-					endColumn: position.column
-				});
+		switch(_type) {
+			case 'jpg':
+			case 'jpeg':
+			case 'bmp':
+			case 'png':
+				var image		= new Image();
+				image.src		= file.file;
+				image.onload	= function onLoad() {
+					_element.appendChild(image);
+				};
 				
-				return require('../Resources/DST-API.json');
-			}
-		});
+				_element.classList.add('image');
+			break;
+			case 'tex':
+				var tex = new Klei();
+				tex.parse(file.content);
+			break;
+			default:
+				monaco.languages.registerCompletionItemProvider('lua', {
+					triggerCharacters:		["."],
+					provideCompletionItems: function(model, position) {
+						var textUntilPosition	= model.getValueInRange({
+							startLineNumber: 1,
+							startColumn: 1,
+							endLineNumber: position.lineNumber,
+							endColumn: position.column
+						});
+						
+						return require('../Resources/DST-API.json');
+					}
+				});
 
-		_editor = monaco.editor.create(_element, {
-			value:					file.content,
-			language:				language,
-			lineNumbers:			true,
-			automaticLayout:		true,
-			roundedSelection:		true,
-			scrollBeyondLastLine:	false,
-			readOnly:				false,
-			folding:				true,
-			tabCompletion:			true,
-			useTabStops:			true,
-			wordWrap:				false, /* Make as View-Settings */
-			theme:					'vs-dark',
-			scrollbar: {
-				useShadows:					false,
-				verticalHasArrows:			true,
-				horizontalHasArrows:		true,
-				vertical:					'visible',
-				horizontal:					'visible',
-				verticalScrollbarSize:		18,
-				horizontalScrollbarSize:	18,
-				arrowSize:					21
-			}
-		});
+				_editor = monaco.editor.create(_element, {
+					value:					file.content,
+					language:				_type,
+					lineNumbers:			true,
+					automaticLayout:		true,
+					roundedSelection:		true,
+					scrollBeyondLastLine:	false,
+					readOnly:				false,
+					folding:				true,
+					tabCompletion:			true,
+					useTabStops:			true,
+					wordWrap:				false, /* Make as View-Settings */
+					theme:					'vs-dark',
+					scrollbar: {
+						useShadows:					false,
+						verticalHasArrows:			true,
+						horizontalHasArrows:		true,
+						vertical:					'visible',
+						horizontal:					'visible',
+						verticalScrollbarSize:		18,
+						horizontalScrollbarSize:	18,
+						arrowSize:					21
+					}
+				});
 		
-		_editor.onDidChangeModelContent(function(event) {
-			_changed = true;
-			document.querySelector('ui-menu button[data-command="save"]').removeAttribute('disabled');
-			document.querySelector('ui-menu button[data-command="save_all"]').removeAttribute('disabled');
-			this.setTitle(_path.basename(_file.file), true);
-		}.bind(this));
-		
-		_editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S, function onSave() {
-			this.save();
-		}.bind(this));
+				_editor.onDidChangeModelContent(function(event) {
+					_changed = true;
+					document.querySelector('ui-menu button[data-command="save"]').removeAttribute('disabled');
+					document.querySelector('ui-menu button[data-command="save_all"]').removeAttribute('disabled');
+					this.setTitle(_path.basename(_file.file), true);
+				}.bind(this));
+				
+				_editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S, function onSave() {
+					this.save();
+				}.bind(this));
+			break;
+		}
 	};
 	
 	this.getFileName = function getFileName() {
@@ -91,6 +114,10 @@ module.exports = (function Editor(IDE) {
 	};
 	
 	this.save = function save() {
+		if(_editor == null) {
+			return;
+		};
+		
 		IPC.send('file:save', {
 			file:		_file.file,
 			content:	_editor.getValue({
@@ -126,7 +153,10 @@ module.exports = (function Editor(IDE) {
 	};
 	
 	this.close = function close() {
-		_editor.dispose();
+		if(_editor != null) {
+			_editor.dispose();
+		};
+		
 		_tabs.removeChild(_tab);
 		_editors.removeChild(_element);
 	};
---
+++ Source/Classes/Klei/Texture.js
@@ -0,0 +1,155 @@
+module.exports = (function Texture() {
+	const Platform = {
+		PC:			12,
+		XBOX360:	11,
+		PS3:		10,
+		Unknown:	0
+	};
+	
+	const PixelFormat = {
+		DXT1:		0,
+		DXT3:		1,
+		DXT5:		2,
+		ARGB:		4,
+		Unknown:	7
+	};
+	
+	const TextureType = {
+		OneD:		1,
+		TwoD:		2,
+		ThreeD:		3,
+		Cubemap:	4
+	};
+	
+	var _platform		= null;
+	var _pixel_format	= null;
+	var _texture_type	= null;
+	var _num_mips		= null;
+	var _flags			= null;
+	var _remainder		= null;
+	var _remainder_old	= null;
+	var _raw			= null;
+	
+	this.parse = function parse(content) {
+		buffer = new Buffer(content);
+		header = buffer.readUInt8();
+		
+		var signature = buffer.toString('utf8', 0, 4);
+		
+		console.log('Signature', signature);
+		
+		console.log('Parse Header');
+		_platform		= header & 15;
+		_pixel_format	= (header >> 4)  & 31;
+		_texture_type	= (header >> 9)  & 15;
+		_num_mips		= (header >> 13) & 31;
+		_flags			= (header >> 18) & 3;
+		_remainder		= (header >> 20) & 4095;
+		_remainder_old	= (header >> 14) & 262143;
+		
+		_raw = buffer.toString('utf8', 4);
+		console.log({
+			_platform:		this.getPlatform(),
+			_pixel_format:	this.getPixelFormat(),
+			_texture_type:	this.getTextureType(),
+			_num_mips:		this.getMipsAvailable(),
+			_flags:			_flags,
+			_remainder:		_remainder,
+			isPreCaveUpdate: this.isPreCaveUpdate()
+		});
+		/*
+
+		// Just a little hack for pre cave updates, can remove later.
+		
+
+		File.Raw = reader.ReadBytes((int)(reader.BaseStream.Length - reader.BaseStream.Position));
+		/
+		*/
+		console.log(this.getMainMipMap());
+	};
+	
+	this.getPlatform = function getPlatform() {
+		var platform = null;
+		
+		Object.keys(Platform).forEach(function(key) {
+			if(Platform[key] == _platform) {
+				platform = key;
+				return;
+			}
+		});
+		
+		return platform;
+	};
+	
+	this.getPixelFormat = function getPixelFormat() {
+		var format = null;
+		
+		Object.keys(PixelFormat).forEach(function(key) {
+			if(PixelFormat[key] == _pixel_format) {
+				format = key;
+				return;
+			}
+		});
+		
+		return format;
+	};
+		
+	this.getTextureType = function getTextureType() {
+		var type = null;
+		
+		Object.keys(TextureType).forEach(function(key) {
+			if(TextureType[key] == _texture_type) {
+				type = key;
+				return;
+			}
+		});
+		
+		return type;
+	};
+	
+	this.getMipsAvailable = function getMipsAvailable() {
+		return _num_mips;
+	};
+	
+	this.isPreCaveUpdate = function isPreCaveUpdate() {
+		return _remainder_old == 262143;
+	};
+	
+	this.getMainMipMap = function getMainMipMap() {
+		var buffer = new Buffer(_raw);
+		
+		console.log({
+			width:		buffer.readUInt16LE(),
+			height:		buffer.readUInt16LE(),
+			pitch:		buffer.readUInt16LE(),
+			size:		buffer.readUInt32LE()
+		});
+
+        //reader.BaseStream.Seek((File.Header.NumMips - 1) * 10, SeekOrigin.Current);
+		//mipmap.Data = reader.ReadBytes((int)mipmap.DataSize);
+	};
+	
+	this.getMipMaps = function getMipMaps() {
+		var entries = new Array(_num_mips);
+		var Readable = require('stream').Readable;
+		var content = new Readable(_raw);
+
+		for(var index = 0; index < _num_mips; index++) {
+			entries[index] = {
+				width:	content.read(16),
+				height:	content.read(16),
+				pitch:	content.read(16),
+				size:	content.read(32)
+			};
+		}
+		
+		console.log(entries);
+		
+		for(var index = 0; index < _num_mips; index++) {
+			entries[index].datta = content.read(entries[index].size);
+		}
+		
+		console.log(entries);
+		return entries;
+	};
+});
