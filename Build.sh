811ea79856eca45e15827f86c0264aed21510328
ThemInspectors <ThemInspectors@users.noreply.github.com>
2017-05-28 18:14:54 -0600

    Update Build.sh
    fix the issues

--- Build.sh
+++ Build.sh
@@ -1,4 +1,5 @@
-INSTALL_DIR="~/.dsted"
+CUR_DIR=`pwd`
+INSTALL_DIR="$HOME/.dsted"
 NAME_SOURCE="Source" #this is redundant
 #UPDATE=true #if set to true it will attempt to grab the latest version and build the update. not going to work on it
 
@@ -6,11 +7,11 @@ echo "This is the Linux install version."
 echo "You need to install nodejs and npm for this to work."
 
 #create the install directory
-mkdir $INSTALL_DIR/$BUILD
+mkdir $INSTALL_DIR
+
 # copy the things
-cp -f pwd/$NAME_SOURCE $INSTALL_DIR/
+cp -rf $CUR_DIR/$NAME_SOURCE $INSTALL_DIR/
 
 ## Go to Build and Start NPM
-cd $INSTALL_DIR
+cd $INSTALL_DIR/$NAME_SOURCE
 npm install
-
